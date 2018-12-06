0058367843cd1f466d7b6615d00d1cccec1f8c64
UndeadFairy <krikes@seznam.cz>
2018-12-06 22:53:24 +0100

    Added images to the template

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,12 @@ from django.utils import timezone
 
 class Post1(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    upload = models.ImageField(upload_to='', default = 'kitten.jpg')
     title = models.CharField(max_length=200)
     text = models.TextField()
     published_date = models.DateTimeField(
             blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url
+from django.conf import settings
 from . import views
 
 urlpatterns = [
      url(r'^$', views.post_list, name='post_list'),
+     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 ]
+
+
+
+
+
+
